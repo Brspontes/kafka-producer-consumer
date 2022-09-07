b9086e7b687438d80280028b52d17643447fc400
Brian Pontes <brian.robert16@hotmail.com>
2022-09-07 17:51:43 -0300

    create project with kafka

---
+++ Consumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Confluent.Kafka;
+
+Console.WriteLine("Start");
+
+var config = new ConsumerConfig
+{
+    GroupId = "desenvolvedor",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("topico-teste");
+while(true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Mensagem teste {Guid.NewGuid().ToString()}"
+};
+
+for (int i = 0; i < 10; i++)
+{
+    var result = await producer.ProduceAsync("topico-teste", message);
+    Console.WriteLine($"{result.Offset}");
+}
