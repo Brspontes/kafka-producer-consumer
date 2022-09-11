a724736ae92e3d0a4b38f2dbf1014a28498a5c91
Brian Pontes <brian.robert16@hotmail.com>
2022-09-11 16:31:42 -0300

    adicionando schemaregistry

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,6 +1,15 @@
 ﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
 
 Console.WriteLine("Start");
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081",
+};
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 var config = new ConsumerConfig
 {
@@ -8,11 +17,14 @@ var config = new ConsumerConfig
     BootstrapServers = "localhost:9092"
 };
 
-using var consumer = new ConsumerBuilder<string, string>(config).Build();
+using var consumer = new ConsumerBuilder<string, desenvolvedor.io.Curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<desenvolvedor.io.Curso>(schemaRegistry)
+    .AsSyncOverAsync())
+    .Build();
 
-consumer.Subscribe("topico-teste");
+consumer.Subscribe("cursos");
 while(true)
 {
     var result = consumer.Consume();
-    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value}");
+    Console.WriteLine($"Mensagem: {result.Message.Key}-{result.Message.Value.Descricao}");
 }
